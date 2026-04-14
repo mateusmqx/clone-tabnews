2dc593dd1946b38a67ce16312b88be00e8c90186
Mateus <144172794+mateusmqx@users.noreply.github.com>
2026-04-14 02:46:27 +0000

    fix `prettier` style linting

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,31 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-    const updatedAt = new Date().toISOString();
+  const updatedAt = new Date().toISOString();
 
-    const datbaseVersionResult = await database.query("SHOW server_version;");
-    const datbaseVersionValue = datbaseVersionResult.rows[0].server_version;
- 
-    const databaseMaxConnectionResult = await database.query("SHOW max_connections;");
-    const databaseMaxConnectionValue = databaseMaxConnectionResult.rows[0].max_connections;
+  const datbaseVersionResult = await database.query("SHOW server_version;");
+  const datbaseVersionValue = datbaseVersionResult.rows[0].server_version;
 
-    const databaseName = process.env.POSTGRES_DB;
-    const databaseOpenedConnectionsResult = await database.query({
-        text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-        values: [databaseName],
-    });
-    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
 
-    response.status(200).json({ 
-        updated_at: updatedAt ,
-        dependencies: {
-            database: {
-                version: datbaseVersionValue,
-                max_connections: parseInt(databaseMaxConnectionValue),
-                opened_connections: databaseOpenedConnectionsValue,
-            }
-        }
-    });
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: datbaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
